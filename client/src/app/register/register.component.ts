bdc4715d21df4b36504dc82ed0bb3eda11463175
Darian Amini <darianamini@gmail.com>
2025-08-27 16:54:12 +0330

    Added home and register pages

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, inject, Output, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountsService } from '../_services/accounts.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  private accountService = inject(AccountsService); 
+  model: any = {};
+  cancelRegister = output<boolean>(); 
+
+  register() {
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        console.log(response); 
+        this.cancel(); 
+      }, 
+      error: error => console.log(error)
+    })
+  } 
+
+  cancel() {
+    this.cancelRegister.emit(false); 
+  }
+}
